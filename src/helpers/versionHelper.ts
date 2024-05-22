888eaa0be37afc40581b9875ed8274325057f347
Vitaliy <vitaliykorzenkoua@gmail.com>
2024-05-22 23:01:27 +0300

    start spread
    start spread

--- src/helpers/versionHelper.ts
+++ src/helpers/versionHelper.ts
@@ -2,7 +2,7 @@
 
 export class VersionHelper {
 
-    private static version: string = '1.0.23(May 18)';
+    private static version: string = '1.0.24(May 22)';
 
     public static getVersion(): string {
         return VersionHelper.version;
