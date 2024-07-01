01de2392c19e64e09927834eb9b2df6f8bd98d8f
Vitaliy <vitaliykorzenkoua@gmail.com>
2024-07-01 16:39:02 +0300

    drive fluent2
    drive fluent2

--- src/helpers/versionHelper.ts
+++ src/helpers/versionHelper.ts
@@ -2,7 +2,7 @@
 
 export class VersionHelper {
 
-    private static version: string = '1.0.29(July 2)';
+    private static version: string = '1.0.30(July 1)';
 
     public static getVersion(): string {
         return VersionHelper.version;
